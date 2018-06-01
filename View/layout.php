62da99de721249c80c38ffb631e3ec9723a65eca
BILGE <bilge.ekinci@etu.univ-lyon1.fr>
2018-06-01 10:13:34 +0200

    modification des fichiers php avec le layout

--- View/layout.php
+++ View/layout.php
@@ -6,7 +6,7 @@
         
         <link rel="stylesheet" href="./css/header.css">
         <link rel="stylesheet" href="./css/convention.css">
-        
+        <link rel="stylesheet" href="css/footer.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
          <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
